aaee142fbce8ea3e22091aad5f9a8cb2b102ee1b
blSirius <siriusblackazka@gmail.com>
2024-01-05 20:57:40 +0700

    5/1

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const { Pool } = require('pg');
 const fs = require('fs');
 const path = require('path')
 const bcrypt = require('bcrypt');
-const { log } = require('console');
 const jwt = require('jsonwebtoken');
+
+
+const secretKey = 'PeemSecert';
 
 const app = express();
 const port = 5000;
@@ -16,6 +18,7 @@ const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use(express.json());
+
 
 const pool = new Pool({
   user: 'postgres',
@@ -56,7 +59,7 @@ app.post('/login', async (req, res) => {
 
     console.log(result.rows[0].username)
 
-    const token = jwt.sign({ username: username }, 'PeemSecret', { expiresIn: '1h' });
+    const token = jwt.sign({ username: username }, secretKey, { expiresIn: '1h' });
     
 
     res.json({ message: 'Login successful' ,token: token });
@@ -66,59 +69,60 @@ app.post('/login', async (req, res) => {
   }
 });
 
-// app.use('/getImageFolder', express.static('labels'));
+app.use('/getImageFolder', express.static('labels'));
 
-// app.get('/getLabelFolder', (req, res) => {
-//   try {
-//     const folders = fs.readdirSync('labels', { withFileTypes: true })
-//       .filter(dirent => dirent.isDirectory())
-//       .map(dirent => dirent.name);
+app.get('/getLabelFolder', (req, res) => {
+  try {
+    const folders = fs.readdirSync('labels', { withFileTypes: true })
+      .filter(dirent => dirent.isDirectory())
+      .map(dirent => dirent.name);
 
-//     res.json({ folders });
-//   } catch (error) {
-//     console.error('Error getting folder names:', error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
+    res.json({ folders });
+  } catch (error) {
+    console.error('Error getting folder names:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-// app.post('/updateImageFolder', upload.single('labels'), async (req, res) => {
-//   try {
-//     const folderName = req.body.folderName || req.query.folderName || 'defaultFolder';
-//     const folderPath = `labels/${folderName}`;
-//     if (!fs.existsSync(folderPath)) {
-//       fs.mkdirSync(folderPath, { recursive: true });
-//     }
-//     const files = fs.readdirSync(folderPath);
-//     const newImageName = `${files.length + 1}.png`;
-//     const imagePath = path.join(folderPath, newImageName);
-//     fs.writeFileSync(imagePath, req.file.buffer);
-//     res.json('Successfully uploaded');
-//   } catch (error) {
-//     console.error('Error uploading image:', error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
+app.post('/updateImageFolder', upload.single('labels'), async (req, res) => {
+  try {
+    const folderName = req.body.folderName || req.query.folderName || 'defaultFolder';
+    const folderPath = `labels/${folderName}`;
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true });
+    }
+    const files = fs.readdirSync(folderPath);
+    const newImageName = `${files.length + 1}.png`;
+    const imagePath = path.join(folderPath, newImageName);
+    fs.writeFileSync(imagePath, req.file.buffer);
+    res.json('Successfully uploaded');
+  } catch (error) {
+    console.error('Error uploading image:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-// app.get('/getTodo', async (req, res) => {
-//   try {
-//     const result = await pool.query('SELECT * FROM todo');
-//     res.json(result.rows);
-//   } catch (error) {
-//     console.error('Error:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
 
-// app.post('/addTodo', async (req, res) => {
-//   const { no, list } = req.body;
-//   try {
-//     const result = await pool.query('INSERT INTO todo (no, list) VALUES ($1, $2) RETURNING *', [no, list]);
-//     res.json(result.rows[0]);
-//   } catch (error) {
-//     console.error('Error:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
+
+
+
+app.post('/decode-token', (req, res) => {
+  const { token } = req.body;
+
+  if (!token) {
+    return res.status(400).json({ error: 'Token not provided' });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+
+    console.log('decoded: ',decoded)
+
+    res.json({ message: 'decoded success' ,decoded: decoded });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
